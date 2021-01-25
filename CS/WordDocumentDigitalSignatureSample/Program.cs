9657052569d5cb28f5c7b0fecc3bb35107b70dfb
geumjaLee <polina.fedorova@devexpress.com>
2021-01-25 18:03:17 +0300

    add validation example

--- CS/WordDocumentDigitalSignatureSample/Program.cs
+++ CS/WordDocumentDigitalSignatureSample/Program.cs
@@ -1,53 +1,57 @@
 ﻿using DevExpress.Office.DigitalSignatures;
 using DevExpress.Office.Tsp;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
+using System.Net;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WordDocumentDigitalSignatureSample
 {
     class Program
     {
+        static string input = "Template.docx";
+        static string output = "Template_signed.docx";
         static void Main(string[] args)
         {
-            SignDocument("Template.docx");
+            //Enable all security protocols:
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12 
+                | SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11;
+ 
+            SignDocument(input);
+            ValidateSignature(output);
         }
         static void SignDocument(string path)
-        {
-            string output = "Template_signed.docx";
+        {            
+            //Sign a document and save the result:
             DocumentSigner documentSigner = new DocumentSigner();
             documentSigner.Sign(path, output, CreateSignatureOptions(), CreateSignatureInfo());
-            Process.Start(output);
         }
-    
+        //Specify signature options
         static SignatureOptions CreateSignatureOptions()
         {
             X509Certificate2 certificate = new X509Certificate2("Certificate/SignDemo.pfx", "dxdemo");
             Uri tsaServer = new Uri("https://freetsa.org/tsr");
-            HashAlgorithmType hashAlgorithm = HashAlgorithmType.SHA256;
             SignatureOptions options = new SignatureOptions();
             options.Certificate = certificate;
-
             if (tsaServer != null)
                 options.TsaClient = new TsaClient(tsaServer, HashAlgorithmType.SHA256);
+
+            //In this example, certificate validation is skipped
+            options.SignatureFlags &= ~SignatureFlags.ValidateCertificate;
+            options.CertificateKeyUsageFlags = X509KeyUsageFlags.None;
+            options.DigestMethod = HashAlgorithmType.SHA256;
 
             X509ChainPolicy policy = new X509ChainPolicy();
             policy.RevocationMode = X509RevocationMode.NoCheck;
             policy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
-            policy.VerificationFlags |= X509VerificationFlags.AllowUnknownCertificateAuthority | 
+            policy.VerificationFlags |= X509VerificationFlags.AllowUnknownCertificateAuthority |
                 X509VerificationFlags.IgnoreCertificateAuthorityRevocationUnknown;
             options.CertificatePolicy = policy;
             options.TimestampCertificatePolicy = policy;
-            options.SignatureFlags &= ~SignatureFlags.ValidateCertificate;
-            options.CertificateKeyUsageFlags = X509KeyUsageFlags.None;
-            options.DigestMethod = hashAlgorithm;
             return options;
         }
+        
+        //Specify signature information:
         static SignatureInfo CreateSignatureInfo()
         {
             SignatureInfo signatureInfo = new SignatureInfo();
@@ -64,6 +68,46 @@ namespace WordDocumentDigitalSignatureSample
             signatureInfo.Comments = "Demo Digital Signature";
             return signatureInfo;
         }
-    
+
+        private static void ValidateSignature(string path)
+        {
+            DocumentSigner validator = new DocumentSigner();
+
+            SignatureValidationOptions validationOptions = new SignatureValidationOptions();
+            
+            //In this example, signature and timestamp certificate validation is skipped
+            validationOptions.ValidationFlags = ~ValidationFlags.ValidateSignatureCertificate & ~ValidationFlags.ValidateTimestampCertificate;
+
+            //Validate the signature:
+            PackageSignatureValidation signatureValidation = validator.Validate(path, validationOptions);
+            string validationMessage = signatureValidation.ResultMessage;
+            
+            //Check validation result and show information in the console:
+            switch (signatureValidation.Result)
+            {
+                case PackageSignatureValidationResult.Valid:
+                    Console.WriteLine(validationMessage); Console.ReadKey();
+                    Process.Start(path);
+                    break;
+
+                case PackageSignatureValidationResult.SignaturesNotFound:
+                    Console.WriteLine(validationMessage);
+                    break;
+
+                case PackageSignatureValidationResult.Invalid:
+                case PackageSignatureValidationResult.PartiallyValid:
+                    var failedCheckDetails = signatureValidation.Items[0].FailedCheckDetails;
+                    Console.WriteLine(validationMessage);
+                    int i = 1;
+                    foreach (SignatureCheckResult checkResult in failedCheckDetails)
+                    {
+                        Console.WriteLine(String.Format("Validation details {0}: \r\n" +
+                            "{1} failed, Info: {2} \r\n", i, checkResult.CheckType, checkResult.Info));
+                        i++;
+                    }
+                    Console.ReadKey();
+                    break;
+            }
+        }
     }
 }
--- VB/WordDocumentDigitalSignatureSample/Program.vb
+++ VB/WordDocumentDigitalSignatureSample/Program.vb
@@ -1,36 +1,40 @@
 ﻿Imports DevExpress.Office.DigitalSignatures
 Imports DevExpress.Office.Tsp
 Imports System
-Imports System.Collections.Generic
 Imports System.Diagnostics
-Imports System.IO
-Imports System.Linq
+Imports System.Net
 Imports System.Security.Cryptography.X509Certificates
-Imports System.Text
-Imports System.Threading.Tasks
 
 Namespace WordDocumentDigitalSignatureSample
 	Friend Class Program
+		Private Shared input As String = "Template.docx"
+		Private Shared output As String = "Template_signed.docx"
 		Shared Sub Main(ByVal args() As String)
-			SignDocument("Template.docx")
+			'Enable all security protocols:
+			ServicePointManager.SecurityProtocol = ServicePointManager.SecurityProtocol Or SecurityProtocolType.Tls12 Or SecurityProtocolType.Ssl3 Or SecurityProtocolType.Tls Or SecurityProtocolType.Tls11
+
+			SignDocument(input)
+			ValidateSignature(output)
 		End Sub
 		Private Shared Sub SignDocument(ByVal path As String)
-			Dim output As String = "Template_signed.docx"
+			'Sign a document and save the result:
 			Dim documentSigner As New DocumentSigner()
 			documentSigner.Sign(path, output, CreateSignatureOptions(), CreateSignatureInfo())
-			Process.Start(output)
 		End Sub
-
+		'Specify signature options
 		Private Shared Function CreateSignatureOptions() As SignatureOptions
 			Dim certificate As New X509Certificate2("Certificate/SignDemo.pfx", "dxdemo")
 			Dim tsaServer As New Uri("https://freetsa.org/tsr")
-			Dim hashAlgorithm As HashAlgorithmType = HashAlgorithmType.SHA256
 			Dim options As New SignatureOptions()
 			options.Certificate = certificate
-
 			If tsaServer IsNot Nothing Then
 				options.TsaClient = New TsaClient(tsaServer, HashAlgorithmType.SHA256)
 			End If
+
+			'In this example, certificate validation is skipped
+			options.SignatureFlags = options.SignatureFlags And Not SignatureFlags.ValidateCertificate
+			options.CertificateKeyUsageFlags = X509KeyUsageFlags.None
+			options.DigestMethod = HashAlgorithmType.SHA256
 
 			Dim policy As New X509ChainPolicy()
 			policy.RevocationMode = X509RevocationMode.NoCheck
@@ -38,15 +42,14 @@ Namespace WordDocumentDigitalSignatureSample
 			policy.VerificationFlags = policy.VerificationFlags Or X509VerificationFlags.AllowUnknownCertificateAuthority Or X509VerificationFlags.IgnoreCertificateAuthorityRevocationUnknown
 			options.CertificatePolicy = policy
 			options.TimestampCertificatePolicy = policy
-			options.SignatureFlags = options.SignatureFlags And Not SignatureFlags.ValidateCertificate
-			options.CertificateKeyUsageFlags = X509KeyUsageFlags.None
-			options.DigestMethod = hashAlgorithm
 			Return options
 		End Function
+
+		'Specify signature information:
 		Private Shared Function CreateSignatureInfo() As SignatureInfo
 			Dim signatureInfo As New SignatureInfo()
 			signatureInfo.CommitmentType = CommitmentType.ProofOfApproval
-			signatureInfo.Time = DateTime.UtcNow
+			signatureInfo.Time = Date.UtcNow
 			signatureInfo.ClaimedRoles.Clear()
 			signatureInfo.ClaimedRoles.Add("Sales Representative")
 			signatureInfo.Country = "USA"
@@ -59,5 +62,38 @@ Namespace WordDocumentDigitalSignatureSample
 			Return signatureInfo
 		End Function
 
+		Private Shared Sub ValidateSignature(ByVal path As String)
+			Dim validator As New DocumentSigner()
+
+			Dim validationOptions As New SignatureValidationOptions()
+
+			'In this example, signature and timestamp certificate validation is skipped
+			validationOptions.ValidationFlags = (Not ValidationFlags.ValidateSignatureCertificate) And Not ValidationFlags.ValidateTimestampCertificate
+
+			'Validate the signature:
+			Dim signatureValidation As PackageSignatureValidation = validator.Validate(path, validationOptions)
+			Dim validationMessage As String = signatureValidation.ResultMessage
+
+			'Check validation result and show information in the console:
+			Select Case signatureValidation.Result
+				Case PackageSignatureValidationResult.Valid
+					Console.WriteLine(validationMessage)
+					Console.ReadKey()
+					Process.Start(path)
+
+				Case PackageSignatureValidationResult.SignaturesNotFound
+					Console.WriteLine(validationMessage)
+
+				Case PackageSignatureValidationResult.Invalid, PackageSignatureValidationResult.PartiallyValid
+					Dim failedCheckDetails = signatureValidation.Items(0).FailedCheckDetails
+					Console.WriteLine(validationMessage)
+					Dim i As Integer = 1
+					For Each checkResult As SignatureCheckResult In failedCheckDetails
+						Console.WriteLine(String.Format("Validation details {0}: " & ControlChars.CrLf & "{1} failed, Info: {2} " & ControlChars.CrLf, i, checkResult.CheckType, checkResult.Info))
+						i += 1
+					Next checkResult
+					Console.ReadKey()
+			End Select
+		End Sub
 	End Class
 End Namespace
